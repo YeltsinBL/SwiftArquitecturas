303e5e252b95897e7d73424a9b5a0626de651cf2
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-01 10:58:02 -0500

    Creación del proyecto

---
+++ Architecture_MVC/Architecture_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture_MVC
+//
+//  Created by YeltsinMacPro13 on 1/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
