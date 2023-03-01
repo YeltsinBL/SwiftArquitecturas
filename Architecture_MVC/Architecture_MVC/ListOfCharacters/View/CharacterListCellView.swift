04e24b87f8edca3a3692b0c6380813f5f209ba57
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-01 12:03:30 -0500

    Creación de la Vista: Table y Cell views

---
+++ Architecture_MVC/Architecture_MVC/ListOfCharacters/View/CharacterListCellView.swift
@@ -0,0 +1,71 @@
+//
+//  CharacterListCellView.swift
+//  Architecture_MVC
+//
+//  Created by YeltsinMacPro13 on 1/03/23.
+//
+
+import Foundation
+import UIKit
+
+class CharacterListCellView: UITableViewCell {
+    
+    let characterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let characterName: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let characterStatus: UILabel = {
+        let status = UILabel()
+        status.numberOfLines = 0
+        status.translatesAutoresizingMaskIntoConstraints = false
+        return status
+    }()
+    
+    let characterSpecie: UILabel = {
+        let specie = UILabel()
+        specie.numberOfLines = 0
+        specie.translatesAutoresizingMaskIntoConstraints = false
+        return specie
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        [characterImageView, characterName, characterStatus, characterSpecie].forEach(addSubview)
+        
+        NSLayoutConstraint.activate([
+            characterImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            characterImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            characterImageView.heightAnchor.constraint(equalToConstant: 80),
+            characterImageView.widthAnchor.constraint(equalToConstant: 80),
+            
+            characterName.leadingAnchor.constraint(equalTo: characterImageView.trailingAnchor, constant: 20),
+            characterName.topAnchor.constraint(equalTo: characterImageView.topAnchor),
+            
+            characterStatus.leadingAnchor.constraint(equalTo: characterName.leadingAnchor),
+            characterStatus.topAnchor.constraint(equalTo: characterName.topAnchor, constant: 8),
+            
+            characterSpecie.leadingAnchor.constraint(equalTo: characterName.leadingAnchor),
+            characterSpecie.topAnchor.constraint(equalTo: characterStatus.bottomAnchor, constant: 8),
+            
+        ])
+        
+    }
+    
+}
---
+++ Architecture_MVC/Architecture_MVC/ListOfCharacters/View/CharacterListView.swift
@@ -0,0 +1,38 @@
+//
+//  CharacterListView.swift
+//  Architecture_MVC
+//
+//  Created by YeltsinMacPro13 on 1/03/23.
+//
+
+import Foundation
+import UIKit
+
+final class CharacterListView: UIView {
+    let charatersTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(CharacterListView.self, forCellReuseIdentifier: "CharacterListCellView")
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        addSubview(charatersTableView)
+        NSLayoutConstraint.activate([
+            charatersTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            charatersTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            charatersTableView.topAnchor.constraint(equalTo: topAnchor),
+            charatersTableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+}
