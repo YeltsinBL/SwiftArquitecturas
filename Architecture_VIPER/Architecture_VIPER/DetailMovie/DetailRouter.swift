6a60665c313389a072d5dbf8125c64e27cbcfc3c
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-03 12:27:54 -0500

    Conectamos los módulos de VIPER en el Router y pasamos una referencia de la vista principal

--- Architecture_VIPER/Architecture_VIPER/DetailMovie/DetailRouter.swift
+++ Architecture_VIPER/Architecture_VIPER/DetailMovie/DetailRouter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+protocol DetailRouting: AnyObject {
+    func showDetail(fromViewController: UIViewController, withMovieId movieId: String)
+}
+
+class DetailRouter: DetailRouting {
+//    unimos los módulos de VIPER
+//    pasamos la referencia de la Vista anterior a esta, la principal
+    func showDetail(fromViewController: UIViewController, withMovieId movieId: String) {
+//        instancias
+        let interactor = DetailInteractor()
+        let presenter = DetailPresenter(movieId: movieId, interactor: interactor, mapper: MapperDatailMovieViewModel())
+        let view = DetailView(presenter: presenter)
+        presenter.ui = view
+        
+        fromViewController.present(view, animated: true)
+        
+    }
+}
