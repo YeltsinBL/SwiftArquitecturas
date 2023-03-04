00823617b921602140e18f7af00941cffd9fa398
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-03 20:40:42 -0500

    Creación de la parte del Model

---
+++ Architecture_MVVM/Architecture_MVVM/Login/Model/APIClient.swift
@@ -0,0 +1,38 @@
+//
+//  APIClient.swift
+//  Architecture_MVVM
+//
+//  Created by YeltsinMacPro13 on 3/03/23.
+//
+
+import Foundation
+
+enum BackendError: String, Error {
+    case invalidEmail = "Comprueba el Email"
+    case invalidPassword = "Comprueba tu password"
+}
+
+final class APIClient {
+    
+    func login(withEmail email: String, password: String) async throws -> User {
+//        Simulando la petición HTTP y esperar 2 segundos
+        try await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+        return try simulateBackendLogic(email: email, password: password)
+    }
+}
+
+//esta parte debería de estar en el Backend
+func simulateBackendLogic(email: String, password: String) throws -> User {
+    guard email == "exercisewithMVVM" else {
+        print("El usuario no es exercisewithMVVM")
+        throw BackendError.invalidEmail
+    }
+    guard password == "1234567890" else {
+        print("La password no es 1234567890")
+        throw BackendError.invalidPassword
+    }
+    print("Success")
+    return .init(name: "exercisewithMVVM",
+                 token: "token_123456789",
+                 sessionStart: .now)
+}
---
+++ Architecture_MVVM/Architecture_MVVM/Login/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Architecture_MVVM
+//
+//  Created by YeltsinMacPro13 on 3/03/23.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let token: String
+    let sessionStart: Date
+}
