164aa412794c5bb0d8d0e79f2bb5dec666fc1bfb
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-03 22:48:32 -0500

    Conexión del ViewModel con el Model

--- Architecture_MVVM/Architecture_MVVM/Login/LoginViewModel.swift
+++ Architecture_MVVM/Architecture_MVVM/Login/LoginViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class LoginViewModel {
+    
+//    referencias
+    let apiClient: APIClient
+    
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+    
+    @MainActor //para que retorne en el hilo principal
+    func userLogin(withEmail email: String,
+                   password: String) {
+//        se ejecutará el método de forma asíncrona
+        Task {
+//            usamos el método que puede lanzar errores
+            do{
+                let userModel = try await apiClient.login(withEmail: email, password: password)
+            } catch let error as BackendError {
+                print(error.localizedDescription)
+            }
+            
+        }
+    }
+    
+}
